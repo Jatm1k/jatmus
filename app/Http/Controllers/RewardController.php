19b0b61558605e2e449f2371f963d0958cd8c1fe
n.kurbatsky <n.kurbatsky@mcart.ru>
2024-09-02 17:59:43 +0700

    ad ads reward

---
+++ app/Http/Controllers/RewardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RewardController extends Controller
+{
+    public function adsReward(Request $request)
+    {
+        $user = User::find($request->userid);
+        $user->balance += 2;
+
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\RewardController;
 use SergiX44\Nutgram\Nutgram;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/webhook', function (Nutgram $bot) {
     $bot->run();
 });
+
+Route::get('/adsreward', [RewardController::class, 'adsReward']);
