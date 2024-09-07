4a2386d15b2d85d88d72946da23cc028ff2b9086
n.kurbatsky <n.kurbatsky@mcart.ru>
2024-09-07 11:25:20 +0700

    fix timer 2

--- resources/js/Components/UI/TimerButton.vue
+++ resources/js/Components/UI/TimerButton.vue
@@ -5,17 +5,26 @@ const props = defineProps(["time"]);
 const emit = defineEmits();
 const timeRemaining = ref(0);
 const timer = ref(null);
+
 const isDisabled = computed(() => {
     return timeRemaining.value > 0;
 });
+
 const formattedTime = computed(() => {
     const minutes = Math.floor(timeRemaining.value / 60);
     const seconds = timeRemaining.value % 60;
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 });
 
+function parseToUTC(dateString) {
+    // Преобразуем строку в формат ISO 8601
+    const isoString = dateString.replace(" ", "T") + "Z";
+    return new Date(isoString);
+}
+
 function startTimer() {
-    const targetTime = Date.parse(props.time) / 1000;
+    // Преобразуем время в UTC
+    const targetTime = parseToUTC(props.time).getTime() / 1000;
     const now = Math.floor(Date.now() / 1000);
     timeRemaining.value = Math.max(0, targetTime - now);
 
@@ -46,7 +55,7 @@ watch(
 </script>
 
 <template>
-    <button class="button ads-button" :disabled="isDisabled">
+    <button :disabled="isDisabled">
         <template v-if="isDisabled">
             {{ formattedTime }}
         </template>
