c23fda6d6501d2da7846f844277a30ce6e54a989
n.kurbatsky <n.kurbatsky@mcart.ru>
2024-09-07 11:30:51 +0700

    fix timer 4

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -78,17 +78,18 @@ function showAdPopupHandler() {
 function addFiveMinutes() {
     const now = new Date();
 
-    now.setMinutes(now.getMinutes() + 5);
+    now.setUTCMinutes(now.getUTCMinutes() + 5);
 
     user.value.next_ad_view = formatDate(now);
 }
+
 function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Месяцы начинаются с 0
-    const day = String(date.getDate()).padStart(2, "0");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Месяцы начинаются с 0
+    const day = String(date.getUTCDate()).padStart(2, "0");
+    const hours = String(date.getUTCHours()).padStart(2, "0");
+    const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(date.getUTCSeconds()).padStart(2, "0");
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
