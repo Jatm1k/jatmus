0d57aadad268a30bb80c021305aa1f598944f67a
Nikolay Kurbatsky <n.kurbatsky@mcart.ru>
2025-02-06 09:33:27 +0700

    замена блока рекламы

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -17,9 +17,6 @@ const tg = window.Telegram.WebApp;
 
 const AdController = window.Adsgram.init({ blockId: "2780" });
 
-AdController.addEventListener("onError", () => {
-    tg.showAlert("Произошла ошибка. Попробуйте ещё раз позже.");
-});
 const showAdPopup = ref(false);
 
 const props = defineProps(["song"]);
@@ -45,7 +42,7 @@ async function processAudio() {
     processing.value = true;
     try {
         if (!user.value.is_premium) {
-            await window.Adsgram.init({ blockId: "int-7170" }).show();
+            await window.Adsgram.init({ blockId: "int-7777" }).show();
         }
     } catch (adError) {
         console.warn("Не удалось показать рекламу:", adError);
